7a5f0ac7113cf8466f8ffd53b41cdedd17e03f36
matthew.renodin <matthew.renodin@gmail.com>
2019-02-13 14:59:48 -0500

    These are updates that allowed me to build on linux

---
+++ ace-toa-rtems--install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+
+	#install mpc	
+	git clone https://github.com/DOCGroup/MPC.git
+	
+	#install ACE_TAO
+	
+	#source the env
+	source setenv
+	echo $ACE_ROOT
+
+	git clone https://github.com/matthew-renodin/ACE_TAO.git
+	
+	
+	#cp platform_mingw32.GNU ACE_TAO/ACE/include/makeinclude/
+	cp platform_macros.GNU ACE_TAO/ACE/include/makeinclude/
+	cp config.h ACE_TAO/ACE/ace
+	cp rTAO.mwc ACE_TAO/TAO
+
+
+	cd ACE_TAO 
+
+	cd ACE
+
+	$ACE_ROOT/bin/mwc.pl ACE.mwc -type gnuace
+	#make all -j8
+
+	#exiting ACE
+	cd ..
+
+
+	cd TAO
+
+	#$ACE_ROOT/bin/mwc.pl rTAO.mwc -type gnuace
+
+	#make all -j8
+	#exiting TAO
+	cd ..
+	
+	#exiting ACE_TAO
+	cd ..
+	
+	
+	echo $ACE_ROOT
+	echo $TAO_ROOT
+	echo $MPC
+
+
+
+
+
---
+++ config.h
@@ -0,0 +1,19 @@
+//#define ACE_LACKS_SYS_TIME_H 
+//#define ACE_LACKS_TIME_H
+//#define ACE_HAS_POSIX_TIME
+//#define ACE_LACKS_NETWORKING
+//#define ACE_HAS_SOCKLEN_T
+#define ACE_LACKS_SBRK
+#define ACE_LACKS_TIMESPEC_T
+#define ACE_HAS_UALARM
+#define ACE_HAS_GETRUSAGE_PROTOTYPE
+#define ACE_HAS_CPU_SET_T
+#define ACE_LAKS_STD_WSTRING
+#define ACE_LACKS_ISCTYPE
+#define ACE_LACKS_GAI_STRERROR
+#define ACE_LACKS_CLOCKID_T
+#define HAVE_STRUCT_TIMESPEC
+#define ACE_LACKS_GETADDRINFO
+#define ACE_MT_SAFE 1 
+#include "ace/config-rtems.h"
+
