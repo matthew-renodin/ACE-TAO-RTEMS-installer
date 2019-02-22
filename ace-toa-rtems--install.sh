28f1e20ab73552f3782c67913664ecfcd854bf0e
matthew.renodin <matthew.renodin@gmail.com>
2019-02-22 11:38:29 -0500

    Updated and tested, but is really calibrated to work on my MINGW

--- ace-toa-rtems--install.sh
+++ ace-toa-rtems--install.sh
@@ -5,11 +5,12 @@
 	#install mpc	
 	git clone https://github.com/DOCGroup/MPC.git
 	
-	#install ACE_TAO
 	
 	#source the env
 	source setenv
 	echo $ACE_ROOT
+	
+	#install ACE_TAO
 
 	git clone https://github.com/matthew-renodin/ACE_TAO.git
 	
@@ -20,14 +21,15 @@
 	cp platform_rtems.x_g++.GNU ACE_TAO/ACE/include/makeinclude/
 	cp config.h ACE_TAO/ACE/ace
 	cp rTAO.mwc ACE_TAO/TAO
-
+	cp mACE.mwc ACE_TAO/ACE
+	
 
 	cd ACE_TAO 
 
 	cd ACE
 
-	$ACE_ROOT/bin/mwc.pl ACE.mwc -type gnuace
-	#make all -j8
+	$ACE_ROOT/bin/mwc.pl mACE.mwc -type gnuace
+	make all
 
 	#exiting ACE
 	cd ..
@@ -35,9 +37,9 @@
 
 	cd TAO
 
-	#$ACE_ROOT/bin/mwc.pl rTAO.mwc -type gnuace
+	$ACE_ROOT/bin/mwc.pl rTAO.mwc -type gnuace
 
-	#make all -j8
+	make all
 	#exiting TAO
 	cd ..
 	
